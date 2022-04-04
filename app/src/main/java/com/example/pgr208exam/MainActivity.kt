85c2d028209e545c03638a1b4ecc470544e1622b
Sebastian Hellum <sebhellum@gmail.com>
2022-04-04 20:20:05 +0200

    FragmentManager and NavController for switching between fragments

--- app/src/main/java/com/example/pgr208exam/MainActivity.kt
+++ app/src/main/java/com/example/pgr208exam/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.pgr208exam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        //onCreate for the main activity
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //FragmentManager and NavController for switching between fragments
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigationView)
+        val navController = navHostFragment.navController
+
+        bottomNavigationView.setupWithNavController(navController)
+
+        //All the fragments
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.selectImageFragment, R.id.imageSearchFragment, R.id.savedResultsFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
