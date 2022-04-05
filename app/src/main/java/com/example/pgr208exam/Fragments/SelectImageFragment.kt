6a59004b680194325ddb3da3f94b4eeb4c6477eb
Sebastian Hellum <sebhellum@gmail.com>
2022-04-05 12:29:55 +0200

    Getting bitmap with images stored on the device

--- app/src/main/java/com/example/pgr208exam/Fragments/SelectImageFragment.kt
+++ app/src/main/java/com/example/pgr208exam/Fragments/SelectImageFragment.kt
@@ -1,14 +1,20 @@
 package com.example.pgr208exam.Fragments
 
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.util.Log
+import android.util.Log.INFO
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.pgr208exam.R
-
+import com.example.pgr208exam.UriToBitmap
+import com.example.pgr208exam.getBitmap
+import java.util.logging.Level.INFO
 
 
 class SelectImageFragment : Fragment() {
@@ -28,10 +34,23 @@ class SelectImageFragment : Fragment() {
         // Inflate the layout for this fragment
         var view = inflater.inflate(R.layout.fragment_select_image, container, false)
         selectImageView = view.findViewById(R.id.imageview_select_image)
+        selectImageView.setOnClickListener(View.OnClickListener {
+            val i = Intent()
+            i.type = "*/*"
+            i.action = Intent.ACTION_GET_CONTENT
+
+            startForResult.launch(i)
+        })
 
         return view
     }
 
+    //Callback for getting image/url stored on the device
+    var startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+
+        val selectedImage : Bitmap = getBitmap(requireContext(), null, it.data?.data.toString(), ::UriToBitmap)
+        selectImageView.setImageBitmap(selectedImage)
+    }
 
 
 }
