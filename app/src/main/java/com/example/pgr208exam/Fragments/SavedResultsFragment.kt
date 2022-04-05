21b628656c9fd0b1a8c70eff18410969b85c5034
aadne <adma007@student.kristiania.no>
2022-04-05 14:36:10 +0200

    added some photos, dynamically updating fragment, and created database

--- app/src/main/java/com/example/pgr208exam/Fragments/SavedResultsFragment.kt
+++ app/src/main/java/com/example/pgr208exam/Fragments/SavedResultsFragment.kt
@@ -5,28 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.LinearLayout.LayoutParams
 import com.example.pgr208exam.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SavedResultsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SavedResultsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    lateinit var linearLayout: LinearLayout
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -34,27 +27,43 @@ class SavedResultsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_saved_results, container, false)
+        var view = inflater.inflate(R.layout.fragment_saved_results, container, false)
+
+        linearLayout = view.findViewById(R.id.linearLayout)
+
+        var firstArray = arrayOf(R.drawable.one, R.drawable.two)
+        var secondArray = arrayOf(R.drawable.three, R.drawable.two, R.drawable.one)
+        var bothArrays = arrayOf(firstArray, secondArray)
+
+        for (item in bothArrays) {
+            val horizontalScroll: LinearLayout = LinearLayout(context)
+            var params: LayoutParams = LayoutParams(
+                LayoutParams.MATCH_PARENT,
+                300
+            )
+            horizontalScroll.layoutParams = params
+            linearLayout.addView(horizontalScroll)
+
+
+            for (item in item) {
+                val imageView: ImageView = ImageView(context)
+
+                val imageParams: LayoutParams = LayoutParams(
+                    100,
+                    250
+                )
+                imageView.layoutParams = imageParams
+                imageView.setImageResource(item)
+                horizontalScroll.addView(imageView)
+            }
+        }
+
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SavedResultsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SavedResultsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
-}
+
--- app/src/main/java/com/example/pgr208exam/MainActivity.kt
+++ app/src/main/java/com/example/pgr208exam/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.pgr208exam
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ContentValues
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
-import androidx.navigation.findNavController
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.io.ByteArrayOutputStream
 
 class MainActivity : AppCompatActivity() {
+
+    private var dbHelper = FeedReaderDbHelper(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         //onCreate for the main activity
@@ -17,14 +23,40 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //FragmentManager and NavController for switching between fragments
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigationView)
         val navController = navHostFragment.navController
 
         bottomNavigationView.setupWithNavController(navController)
 
         //All the fragments
-        val appBarConfiguration = AppBarConfiguration(setOf(R.id.selectImageFragment, R.id.imageSearchFragment, R.id.savedResultsFragment))
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.selectImageFragment,
+                R.id.imageSearchFragment,
+                R.id.savedResultsFragment
+            )
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
+
+        val imagesArray = intArrayOf(R.drawable.one, R.drawable.two, R.drawable.three)
+
+        for (item in imagesArray) {
+            dbHelper.writableDatabase.insert("images", null, ContentValues().apply {
+                put("image", bitArray(item))
+            })
+        }
+
+    }
+
+    fun bitArray(x: Int): ByteArray {
+        //Dummy data in SQLite
+        val bitmap = (x as BitmapDrawable).bitmap
+        val stream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+        val bitmapData = stream.toByteArray()
+
+        return bitmapData;
     }
 }
