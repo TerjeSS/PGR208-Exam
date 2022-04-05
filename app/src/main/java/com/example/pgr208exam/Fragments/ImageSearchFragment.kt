04ffa4c7e2bd9adceaf44769b45ff7874477167d
terje <terjeslaatto@gmail.com>
2022-04-05 20:21:48 +0200

    Something something clever here

--- app/src/main/java/com/example/pgr208exam/Fragments/ImageSearchFragment.kt
+++ app/src/main/java/com/example/pgr208exam/Fragments/ImageSearchFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pgr208exam.Constants
+import com.example.pgr208exam.ItemAdapter
 import com.example.pgr208exam.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -18,16 +23,19 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ImageSearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+
+    val dummyData = Constants.getDummyData()
+
+    var itemAdapter:  ItemAdapter? = null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+    }
+
+    val imageClickListener = View.OnClickListener {
+        Toast.makeText(activity , "I AM CLICKED", Toast.LENGTH_LONG).show()
     }
 
     override fun onCreateView(
@@ -35,26 +43,15 @@ class ImageSearchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_image_search, container, false)
+
+        itemAdapter = ItemAdapter(dummyData, imageClickListener);
+        val view: View = inflater.inflate(R.layout.fragment_image_search, container, false)
+        val recyclerView : RecyclerView = view.findViewById<RecyclerView>(R.id.rc_view)
+        recyclerView.layoutManager = GridLayoutManager(activity, 2)
+        recyclerView.adapter = itemAdapter;
+
+        return view;
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ImageSearchFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ImageSearchFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
