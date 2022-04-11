78b85d5a4c9c4fd27559cc5520165720fb67a0e7
terje <terjeslaatto@gmail.com>
2022-04-11 11:25:53 +0200

    fdsafds

--- app/src/main/java/com/example/pgr208exam/Fragments/ImageSearchFragment.kt
+++ app/src/main/java/com/example/pgr208exam/Fragments/ImageSearchFragment.kt
@@ -1,21 +1,44 @@
 package com.example.pgr208exam.Fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.JSONObjectRequestListener
 import com.example.pgr208exam.Constants
 import com.example.pgr208exam.ItemAdapter
 import com.example.pgr208exam.R
-import kotlinx.coroutines.NonDisposableHandle.parent
+import org.json.JSONObject
+
 
 class ImageSearchFragment : Fragment() {
 
     val dummyData = Constants.getDummyData()
+    val googleURL: String = "http://api-edu.gtl.ai/api/v1/imagesearch/google?url=";
+    val bingURL: String = "http://api-edu.gtl.ai/api/v1/imagesearch/bing?url=";
+    val tineyeURL: String = "http://api-edu.gtl.ai/api/v1/imagesearch/tineye?url=";
+
+    fun getImages(url: String) {
+        AndroidNetworking.get("http://api-edu.gtl.ai/api/v1/imagesearch/bing?url=https://gtl-bucket.s3.amazonaws.com/13132cec1fe04c06b4f0b40d9fc8ecb3.jpg")
+            .build()
+            .getAsJSONObject(object : JSONObjectRequestListener {
+                override fun onResponse(response: JSONObject) {
+                    response.keys()
+
+                    }
+                }
+
+                override fun onError(anError: ANError) {
+                    Log.i("error", "there was an error $anError" )
+                }
+            })
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +54,7 @@ class ImageSearchFragment : Fragment() {
         val recyclerView : RecyclerView = view.findViewById<RecyclerView>(R.id.rc_view)
         recyclerView.layoutManager = GridLayoutManager(context, 3)
         recyclerView.adapter = ItemAdapter(dummyData, requireContext())
-
+        getImages(bingURL);
         return view;
     }
 
