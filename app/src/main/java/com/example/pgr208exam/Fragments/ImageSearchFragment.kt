62bac69c16c35408cdd988f1653f572134e8c900
terje <terjeslaatto@gmail.com>
2022-04-12 10:27:12 +0200

    need to mess around in SelectImage

--- app/src/main/java/com/example/pgr208exam/Fragments/ImageSearchFragment.kt
+++ app/src/main/java/com/example/pgr208exam/Fragments/ImageSearchFragment.kt
@@ -13,47 +13,32 @@ import androidx.recyclerview.widget.RecyclerView
 import com.androidnetworking.AndroidNetworking
 import com.androidnetworking.error.ANError
 import com.androidnetworking.interfaces.JSONArrayRequestListener
-import com.androidnetworking.interfaces.JSONObjectRequestListener
 import com.example.pgr208exam.Constants
 import com.example.pgr208exam.ItemAdapter
 import com.example.pgr208exam.R
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import org.json.JSONArray
 import org.json.JSONObject
-import com.example.pgr208exam.ImageObject as ImageObject1
+import kotlin.concurrent.thread
 
 
 class ImageSearchFragment : Fragment() {
 
-    val dummyData = Constants.getDummyData()
+    private val dummyData = Constants.getDummyData()
     val googleURL: String = "http://api-edu.gtl.ai/api/v1/imagesearch/google?url=";
     val bingURL: String = "http://api-edu.gtl.ai/api/v1/imagesearch/bing?url=";
     val tineyeURL: String = "http://api-edu.gtl.ai/api/v1/imagesearch/tineye?url=";
+    val imageList: ArrayList<String> = ArrayList()
 
-
-
-    val jsonImage = """
-    {
-        "store_link": "https://hymanltd.com/vehicles/6773-1966-ford-mustang-gt-coupe/",
-        "name": "1966 Ford Mustang GT Coupe | Hyman Ltd.",
-        "domain": "hymanltd.com",
-        "identifier": "bing",
-        "tracking_id": "None",
-        "thumbnail_link": "https://th.bing.com/th/id/OIP.m6CXdg6dLGPUJJw0IzoZlAHaE8?pid=ImgDet&w=211&h=140&c=7",
-        "description": "",
-        "image_link": "https://th.bing.com/th/id/OIP.m6CXdg6dLGPUJJw0IzoZlAHaE8?pid=ImgDet&w=211&h=140&c=7",
-        "current_date": "None"
-    }""";
-
-    fun getImages(url: String) {
+    private fun getImages(url: String): ArrayList<String> {
+        Log.i("test", "test")
         AndroidNetworking.get("http://api-edu.gtl.ai/api/v1/imagesearch/bing?url=https://gtl-bucket.s3.amazonaws.com/13132cec1fe04c06b4f0b40d9fc8ecb3.jpg")
             .build()
             .getAsJSONArray(object : JSONArrayRequestListener {
                 override fun onResponse(response: JSONArray) {
-                    for(index in 0 until response.length()){
+                    for (index in 0 until response.length()) {
                         val imageURL = (response.get(index) as JSONObject).getString("image_link")
-                        Log.i("imageurl", imageURL)
+                        imageList?.add(index, imageURL)
+                        Log.i("added", "image $imageURL added at index $index")
                     }
 
                 }
@@ -62,7 +47,9 @@ class ImageSearchFragment : Fragment() {
                     Log.i("error", "there was an error $anError")
                 }
             }
+
             )
+return imageList
     }
 
 
@@ -77,6 +64,8 @@ class ImageSearchFragment : Fragment() {
                 Log.i("Result in new fragment", result)
             }
         }
+
+        //Function to GET images from server
     }
 
     override fun onCreateView(
@@ -84,13 +73,13 @@ class ImageSearchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-
         // Inflate the layout for this fragment
+        val list = getImages("");
         val view: View = inflater.inflate(R.layout.fragment_image_search, container, false)
-        val recyclerView : RecyclerView = view.findViewById<RecyclerView>(R.id.rc_view)
+        val recyclerView: RecyclerView = view.findViewById<RecyclerView>(R.id.rc_view)
         recyclerView.layoutManager = GridLayoutManager(context, 3)
-        recyclerView.adapter = ItemAdapter(dummyData, requireContext())
-        getImages(bingURL);
+        //recyclerView.adapter = imageList?.let { ItemAdapter(it, requireContext()) }
+        recyclerView.adapter = ItemAdapter(list, requireContext())
         return view;
     }
 
