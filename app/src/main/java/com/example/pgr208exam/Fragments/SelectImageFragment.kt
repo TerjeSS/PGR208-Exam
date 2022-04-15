74246a824865fb8fe3329b183aab6cf0d02ef758
Sebastian Hellum <sebhellum@gmail.com>
2022-04-15 12:06:49 +0200

    Option to rotate images

--- app/src/main/java/com/example/pgr208exam/Fragments/SelectImageFragment.kt
+++ app/src/main/java/com/example/pgr208exam/Fragments/SelectImageFragment.kt
@@ -18,6 +18,7 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.graphics.rotationMatrix
+import androidx.core.graphics.scale
 import androidx.core.graphics.set
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
@@ -43,6 +44,8 @@ class SelectImageFragment : Fragment() {
     lateinit var imageUri: String
     lateinit var selectTextView: TextView
     lateinit var uploadButton: Button
+    lateinit var rotateLeftButton: Button
+    lateinit var rotateRightButton: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -61,16 +64,14 @@ class SelectImageFragment : Fragment() {
         selectImageView = view.findViewById(R.id.imageview_select_image)
         selectTextView = view.findViewById(R.id.textview_select_image)
         uploadButton = view.findViewById(R.id.upload_button)
+        rotateLeftButton = view.findViewById(R.id.rotate_left_button)
+        rotateRightButton = view.findViewById(R.id.rotate_right_button)
 
         //animation on icon
         selectImageView.animate().apply {
-            duration = 3000
-            scaleX(0.6F)
-            scaleY(0.6F)
-            rotationX(1080f)
+            duration = 2000
+            rotationX(360f)
         }.start()
-
-
 
         uploadButton.setOnClickListener(View.OnClickListener {
             //Ask the user for access to manage all files on the device
@@ -122,12 +123,23 @@ class SelectImageFragment : Fragment() {
             //Adding elements after successfully adding image
             selectTextView.text = "Image is ready, now you can crop it "
             uploadButton.visibility = View.VISIBLE
+            rotateLeftButton.visibility = View.VISIBLE
+            rotateRightButton.visibility = View.VISIBLE
             selectImageView.setBackgroundResource(android.R.color.transparent);
-
 
 
             if (it.resultCode != Activity.RESULT_OK) {
                 selectTextView.text = "Something went wrong 🙄"
+            }
+
+            //OnClick to rotate image to the left
+            rotateLeftButton.setOnClickListener() {
+                selectImageView.rotateImage(-90)
+            }
+
+            //OnClick to rotate image to the right
+            rotateRightButton.setOnClickListener() {
+                selectImageView.rotateImage(90)
             }
 
 
@@ -135,7 +147,7 @@ class SelectImageFragment : Fragment() {
             uploadButton.setOnClickListener() {
                 //Bitmap of cropped image
                 val croppedBitmap : Bitmap = selectImageView.croppedImage
-                selectImageView.setBackgroundResource(R.drawable.ic_image_search)
+                selectImageView.setImageBitmap(croppedBitmap)
 
 
                 //Creating a jpeg-file of the bitmap and saving it on the device
@@ -172,7 +184,10 @@ class SelectImageFragment : Fragment() {
 
                             //Updating UI
                             selectTextView.text = "Image is uploaded 👍"
+                            rotateLeftButton.visibility = View.GONE
+                            rotateRightButton.visibility = View.GONE
                             uploadButton.visibility = View.GONE
+                            selectImageView.setGuidelines(0)
                         }
 
                         override fun onError(anError: ANError) {
