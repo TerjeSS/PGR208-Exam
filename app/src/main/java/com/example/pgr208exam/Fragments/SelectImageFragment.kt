2370c828f019c09047fdabc5f89b725a9bbf452b
Sebastian Hellum <75560412+SebastianHellum@users.noreply.github.com>
2022-04-15 12:33:36 +0200

    Seb test branch (#5)
    * SelectImageView not crashing in fragment_select_image 🟢
    
    * Getting bitmap with images stored on the device
    
    * Getting the imageURI
    
    * Showing elements if imageresult is OK.
    
    * Uses-permission for internet
    
    * Sending imageuri and get 200 ok, but no result.
    
    * Uploading image and fetching URL from server is working 🟢
    
    * Styles is looking decent 🖼️
    
    * Cleaning the code
    
    * Removing unused method
    
    * compile- and targetSdk = 32
    
    * Sending URL response to ImageSearchFragment
    
    * Fixing permission to access files
    
    * Fixing permission to access files
    
    * Fixing permission to access files (fix)
    
    * Added image cropper to manifest and gradle
    
    * Changes in styles
    
    * Cropping images is working, but need to remove guidelines after uploading them 🖼️
    
    * Animation when opening SelectImageFragment
    
    * Option to rotate images
    
    * Removing image from imageView after uploading

--- app/src/main/java/com/example/pgr208exam/Fragments/SelectImageFragment.kt
+++ app/src/main/java/com/example/pgr208exam/Fragments/SelectImageFragment.kt
@@ -15,9 +15,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.graphics.rotationMatrix
+import androidx.core.graphics.scale
+import androidx.core.graphics.scaleMatrix
+import androidx.core.graphics.set
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResult
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResult
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
@@ -26,6 +33,11 @@ import com.androidnetworking.AndroidNetworking
 import com.androidnetworking.common.Priority
 import com.androidnetworking.error.ANError
 import com.androidnetworking.interfaces.OkHttpResponseAndStringRequestListener
+import com.edmodo.cropper.CropImageView
+import com.example.pgr208exam.BuildConfig
+import com.example.pgr208exam.R
+import com.example.pgr208exam.UriToBitmap
+import com.example.pgr208exam.getBitmap
 import com.example.pgr208exam.*
 import okhttp3.Response
 import java.io.File
@@ -34,11 +46,13 @@ import java.io.FileOutputStream
 
 class SelectImageFragment : Fragment() {
     var listOfUrls: ArrayList<String> = ArrayList();
-    lateinit var selectImageView: ImageView
+    lateinit var selectImageView: CropImageView
     lateinit var imageUri: String
     lateinit var selectTextView: TextView
     lateinit var uploadButton: Button
-    private val viewModel: SharedViewModel by activityViewModels()
+    lateinit var rotateLeftButton: Button
+    lateinit var rotateRightButton: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,25 +60,32 @@ class SelectImageFragment : Fragment() {
         }
     }
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-
-
         // Inflate the layout for this fragment
         var view = inflater.inflate(R.layout.fragment_select_image, container, false)
         selectImageView = view.findViewById(R.id.imageview_select_image)
         selectTextView = view.findViewById(R.id.textview_select_image)
         uploadButton = view.findViewById(R.id.upload_button)
+        rotateLeftButton = view.findViewById(R.id.rotate_left_button)
+        rotateRightButton = view.findViewById(R.id.rotate_right_button)
 
-        selectImageView.setOnClickListener(View.OnClickListener {
+        //animation on icon
+        selectImageView.animate().apply {
+            duration = 2000
+            rotationX(360f)
+        }.start()
+
+        uploadButton.setOnClickListener(View.OnClickListener {
             //Ask the user for access to manage all files on the device
             requestPermission()
             val i = Intent()
-            i.type = "*/*"
+            i.type = "image/*"
             i.action = Intent.ACTION_GET_CONTENT
+            uploadButton.text = "upload"
             startForResult.launch(i)
         })
         return view
@@ -97,70 +118,103 @@ class SelectImageFragment : Fragment() {
     }
 
 
-                    //Callback for getting image/url stored on the device
-                    var startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-                        if (it.resultCode == Activity.RESULT_OK) {
-                            imageUri = it.data?.data.toString()
-                            Log.i("This is the image URI", imageUri)
-                            val selectedImage: Bitmap = getBitmap(requireContext(), null, imageUri, ::UriToBitmap)
-                            selectImageView.setImageBitmap(selectedImage)
+    //Callback for getting image/url stored on the device
+    var startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == Activity.RESULT_OK) {
+            imageUri = it.data?.data.toString()
+            Log.i("This is the image URI", imageUri)
+            val selectedImage: Bitmap = getBitmap(requireContext(), null, imageUri, ::UriToBitmap)
+            selectImageView.setImageBitmap(selectedImage)
 
-                            //Adding elements after successfully adding image
-                            selectTextView.text = "Image is ready, now you can upload!"
-                            uploadButton.visibility = View.VISIBLE
-
-
-                            if (it.resultCode != Activity.RESULT_OK) {
-                                selectTextView.text = "Something went wrong 🙄"
-                            }
-
-                            //Creating a jpeg-file of the bitmap and saving it on the device
-                            val filename = "selectedImage.jpeg"
-                            val sd = Environment.getExternalStorageDirectory()
-                            val dest = File(sd, filename)
-                            Log.i("This is the jpeg", dest.absolutePath)
-
-                            try {
-                                val out = FileOutputStream(dest)
-                                selectedImage.compress(Bitmap.CompressFormat.JPEG, 15, out)
-                                out.flush()
-                                out.close()
-                            } catch (e: Exception) {
-                                e.printStackTrace()
-                            }
+            //Adding elements after successfully adding image
+            selectTextView.text = "Image is ready, now you can crop it "
+            uploadButton.visibility = View.VISIBLE
+            rotateLeftButton.visibility = View.VISIBLE
+            rotateRightButton.visibility = View.VISIBLE
+            selectImageView.setBackgroundResource(android.R.color.transparent);
 
 
-                            //OnClick to upload image to the server
-                            uploadButton.setOnClickListener() {
+            if (it.resultCode != Activity.RESULT_OK) {
+                selectTextView.text = "Something went wrong 🙄"
+            }
+
+            //OnClick to rotate image to the left
+            rotateLeftButton.setOnClickListener() {
+                selectImageView.rotateImage(-90)
+            }
+
+            //OnClick to rotate image to the right
+            rotateRightButton.setOnClickListener() {
+                selectImageView.rotateImage(90)
+            }
 
 
-                                Log.i("Button click", "Upload button got clicked")
-                                AndroidNetworking.upload("http://api-edu.gtl.ai/api/v1/imagesearch/upload")
-                                    .addMultipartFile("image", dest)
-                                    .addMultipartParameter("Content-Type", "image/jpeg")
-                                    .setTag("uploadTest")
-                                    .setPriority(Priority.HIGH)
-                                    .build()
-                                    .getAsOkHttpResponseAndString(object : OkHttpResponseAndStringRequestListener {
-                                        override fun onResponse(okHttpResponse: Response, response: String) {
-                                            Log.i("This is the OK code", okHttpResponse.toString())
-                                            Log.i("This is the response", response)
+            //OnClick to crop and upload to the server
+            uploadButton.setOnClickListener() {
+                //Bitmap of cropped image
+                val croppedBitmap : Bitmap = selectImageView.croppedImage
+                selectImageView.setImageBitmap(croppedBitmap)
 
 
-                                            viewModel.changeResponseFromPost(response)
-                                            //Updating UI
-                                            selectTextView.text = "Image is uploaded 👍"
-                                            uploadButton.visibility = View.GONE
-                                        }
+                //Creating a jpeg-file of the bitmap and saving it on the device
+                val filename = "selectedImage.jpeg"
+                val sd = Environment.getExternalStorageDirectory()
+                val dest = File(sd, filename)
+                Log.i("This is the jpeg", dest.absolutePath)
 
-                                        override fun onError(anError: ANError) {
-                                            //Error handling when not uploading pics
-                                            uploadButton.visibility = View.GONE
-                                            selectTextView.text = "Could not send. Did you allow access to files?"
-                                            Log.i("This is the error", anError.errorBody)
-                                        }
-                                    })
-                            }
-                        }
-                    }
+                try {
+                    val out = FileOutputStream(dest)
+                    croppedBitmap.compress(Bitmap.CompressFormat.JPEG, 15, out)
+                    out.flush()
+                    out.close()
+                } catch (e: Exception) {
+                    e.printStackTrace()
                 }
+
+                //Upload
+                Log.i("Button click", "Upload button got clicked")
+                AndroidNetworking.upload("http://api-edu.gtl.ai/api/v1/imagesearch/upload")
+                    .addMultipartFile("image", dest)
+                    .addMultipartParameter("Content-Type", "image/jpeg")
+                    .setTag("uploadTest")
+                    .setPriority(Priority.HIGH)
+                    .build()
+                    .getAsOkHttpResponseAndString(object : OkHttpResponseAndStringRequestListener {
+                        override fun onResponse(okHttpResponse: Response, response: String) {
+                            Log.i("This is the OK code", okHttpResponse.toString())
+                            Log.i("This is the response", response)
+
+                            //Sending result to ImageSearchFragment
+                            val result = response                  
+                            viewModel.changeResponseFromPost(response)
+                            
+                            //Updating UI
+                            selectTextView.text = "Image is uploaded 👍"
+                            rotateLeftButton.visibility = View.GONE
+                            rotateRightButton.visibility = View.GONE
+                            uploadButton.visibility = View.GONE
+                            selectImageView.setGuidelines(0)
+                            selectImageView.imageResource = android.R.color.transparent
+                            selectImageView.setBackgroundResource(R.drawable.icon)
+
+                            //animation on icon
+                            selectImageView.animate().apply {
+                                duration = 2000
+                                rotationY(720f)
+                                scaleXBy(-0.4F)
+                                scaleYBy(-0.4F)
+                            }.start()
+                        }
+
+                        override fun onError(anError: ANError) {
+                            //Error handling when not uploading pics
+                            uploadButton.visibility = View.GONE
+                            rotateLeftButton.visibility = View.GONE
+                            rotateRightButton.visibility = View.GONE
+                            selectTextView.text = "Could not send. Did you allow access to files?"
+                            Log.i("This is the error", anError.errorBody)
+                        }
+                    })
+            }
+        }
+    }
