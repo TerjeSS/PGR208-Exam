b5de8264370834e6a911c407effc62626d5dc7b4
aadne <adma007@student.kristiania.no>
2022-04-19 13:48:33 +0200

    Fragment now runs thread on database queries

--- app/src/main/java/com/example/pgr208exam/Fragments/SavedResultsFragment.kt
+++ app/src/main/java/com/example/pgr208exam/Fragments/SavedResultsFragment.kt
@@ -1,28 +1,25 @@
 package com.example.pgr208exam.Fragments
 
-import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.widget.*
 import androidx.core.graphics.drawable.toBitmap
-import androidx.core.graphics.scaleMatrix
-import androidx.core.view.get
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.example.pgr208exam.FullScreenImage
 import com.example.pgr208exam.HorizontalAdapter
 import com.example.pgr208exam.MainActivity
 import com.example.pgr208exam.R
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import java.io.ByteArrayOutputStream
+import kotlin.concurrent.thread
 
 class SavedResultsFragment : Fragment() {
 
@@ -49,80 +46,57 @@ class SavedResultsFragment : Fragment() {
 
         linearLayout = view.findViewById(R.id.linearLayout)
 
-        val spinner: Spinner = view.findViewById(R.id.sortSpinner)
-        ArrayAdapter.createFromResource(
-            requireContext(),
-            R.array.sorting_array,
-            android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            spinner.adapter = adapter
-        }
-
-        val sortingArray = resources.getStringArray(R.array.sorting_array)
-        val orderCursor = ((activity as MainActivity).getImage("resultsNumber"))
+        val sortingArray: Array<out String> = resources.getStringArray(R.array.sorting_array)
         val orderArray: MutableList<Int> = arrayListOf()
-
-        while (orderCursor.moveToNext()) {
-            val original = orderCursor.getInt(orderCursor.getColumnIndexOrThrow("original"))
-            orderArray.add(original)
-        }
-        val originalsCursor =
-            ((activity as MainActivity).getImage("originals"))
-        val resultsCursor =
-            ((activity as MainActivity).getImage("results"))
-
         val originalsArrayNewest: MutableList<OriginalImage> = arrayListOf()
         var originalsArrayOldest: MutableList<OriginalImage> = arrayListOf()
         val originalsArraySize: MutableList<OriginalImage> = arrayListOf()
         val resultsArray: MutableList<ResultsImage> = arrayListOf()
 
-        while (originalsCursor.moveToNext()) {
-            val imageId = originalsCursor.getInt(0)
-            val image: ByteArray =
-                originalsCursor.getBlob(originalsCursor.getColumnIndexOrThrow("image"))
-            val image2: Bitmap = BitmapFactory.decodeByteArray(image, 0, image.size)
-            originalsArrayNewest += (OriginalImage(imageId, image2))
-        }
+        val createViewThread = Thread(Runnable {
 
-        originalsCursor.moveToFirst()
+            val spinner: Spinner = view.findViewById(R.id.sortSpinner)
 
-        while (originalsCursor.moveToNext()) {
-            val imageId = originalsCursor.getInt(0)
-            val image: ByteArray =
-                originalsCursor.getBlob(originalsCursor.getColumnIndexOrThrow("image"))
-            val image2: Bitmap = BitmapFactory.decodeByteArray(image, 0, image.size)
-            originalsArrayOldest = (arrayListOf(OriginalImage(imageId, image2)) + originalsArrayOldest) as MutableList<OriginalImage>
-        }
+            createLayout(
+                sortingArray,
+                originalsArrayNewest,
+                originalsArrayOldest,
+                originalsArraySize,
+                resultsArray,
+                orderArray,
+                spinner
+            )
 
-        originalsCursor.moveToFirst()
+            requireActivity().runOnUiThread(java.lang.Runnable {
+                ArrayAdapter.createFromResource(
+                    requireContext(),
+                    R.array.sorting_array,
+                    android.R.layout.simple_spinner_item
+                ).also { adapter ->
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                    spinner.adapter = adapter
+                }
+            })
+        })
 
-        while (originalsCursor.moveToNext()) {
-            val imageId = originalsCursor.getInt(0)
-            val image: ByteArray =
-                originalsCursor.getBlob(originalsCursor.getColumnIndexOrThrow("image"))
-            val image2: Bitmap = BitmapFactory.decodeByteArray(image, 0, image.size)
-            for (item in orderArray) {
-                originalsArraySize += OriginalImage(-1, image2)
-            }
+        createViewThread.start()
 
-            if (orderArray.contains(imageId)) {
-                originalsArraySize[orderArray.indexOf(imageId)] =
-                    OriginalImage(imageId, image2)
-            } else {
-                originalsArraySize += (OriginalImage(imageId, image2))
-            }
-        }
+        return view
+    }
 
-        while (resultsCursor.moveToNext()) {
-            val imageId = resultsCursor.getInt(0)
-            val image: ByteArray =
-                resultsCursor.getBlob(resultsCursor.getColumnIndexOrThrow("image"))
-            val original = resultsCursor.getInt(3)
-            val image2: Bitmap = BitmapFactory.decodeByteArray(image, 0, image.size)
-            resultsArray += ResultsImage(imageId, image2, original)
-        }
-
+    fun createLayout(
+        sortingArray: Array<out String>,
+        originalsArrayNewest: MutableList<OriginalImage>,
+        originalsArrayOldest: MutableList<OriginalImage>,
+        originalsArraySize: MutableList<OriginalImage>,
+        resultsArray: MutableList<ResultsImage>,
+        orderArray: MutableList<Int>,
+        spinner: Spinner
+    ) {
+        fetchData(
+            orderArray, originalsArrayNewest, originalsArrayOldest, originalsArraySize,
+            resultsArray
+        )
 
         spinner.onItemSelectedListener = object :
             AdapterView.OnItemSelectedListener {
@@ -174,45 +148,96 @@ class SavedResultsFragment : Fragment() {
                         RecyclerView.LayoutParams.WRAP_CONTENT
                     )
                     recyclerView.layoutParams = recyclerParams
-                    recyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+                    recyclerView.layoutManager =
+                        LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
                     newLayout.addView(recyclerView)
 
                     val randomArray: ArrayList<ResultsImage> = arrayListOf()
 
                     for (newItem in resultsArray) {
                         if (newItem.original == item.id) {
-                            /*val newImageView: ImageView = ImageView(context)
-                            val newImageParams: LinearLayout.LayoutParams =
-                                LinearLayout.LayoutParams(
-                                    250,
-                                    ViewGroup.LayoutParams.WRAP_CONTENT
-                                )
-                            newImageView.layoutParams = newImageParams
-                            newImageView.setOnClickListener {
-                                val bitmap = newImageView.drawable.toBitmap()
-                                val stream = ByteArrayOutputStream()
-                                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
-                                val byteArray = stream.toByteArray()
-                                val intent = Intent(requireContext(), FullScreenImage().javaClass)
-                                intent.putExtra("bitmapImage", byteArray)
-                                requireContext().startActivity(intent)
-                            }*/
-
                             randomArray += newItem
                         }
                     }
                     recyclerView.adapter = HorizontalAdapter(randomArray, requireContext())
                 }
-
             }
-
 
             override fun onNothingSelected(parent: AdapterView<*>) {
                 // Another interface callback
             }
         }
+    }
 
-        return view
+    fun fetchData(
+        orderArray: MutableList<Int>,
+        originalsArrayNewest: MutableList<OriginalImage>,
+        originalsArrayOldest: MutableList<OriginalImage>,
+        originalsArraySize: MutableList<OriginalImage>,
+        resultsArray: MutableList<ResultsImage>,
+    ) {
+
+        val orderCursor = ((activity as MainActivity).getImage("resultsNumber"))
+
+        while (orderCursor.moveToNext()) {
+            val original = orderCursor.getInt(orderCursor.getColumnIndexOrThrow("original"))
+            orderArray.add(original)
+        }
+        val originalsCursor =
+            ((activity as MainActivity).getImage("originals"))
+        val resultsCursor =
+            ((activity as MainActivity).getImage("results"))
+
+        while (originalsCursor.moveToNext()) {
+            val imageId = originalsCursor.getInt(0)
+            val image: ByteArray =
+                originalsCursor.getBlob(originalsCursor.getColumnIndexOrThrow("image"))
+            val image2: Bitmap = BitmapFactory.decodeByteArray(image, 0, image.size)
+            originalsArrayNewest += (OriginalImage(imageId, image2))
+        }
+
+        originalsCursor.moveToLast()
+
+        while (originalsCursor.moveToPrevious()) {
+            val imageId = originalsCursor.getInt(0)
+            val image: ByteArray =
+                originalsCursor.getBlob(originalsCursor.getColumnIndexOrThrow("image"))
+            val image2: Bitmap = BitmapFactory.decodeByteArray(image, 0, image.size)
+            originalsArrayOldest += (arrayListOf(
+                OriginalImage(
+                    imageId,
+                    image2
+                )
+            ))
+        }
+
+        originalsCursor.moveToFirst()
+
+        while (originalsCursor.moveToNext()) {
+            val imageId = originalsCursor.getInt(0)
+            val image: ByteArray =
+                originalsCursor.getBlob(originalsCursor.getColumnIndexOrThrow("image"))
+            val image2: Bitmap = BitmapFactory.decodeByteArray(image, 0, image.size)
+            for (item in orderArray) {
+                originalsArraySize += OriginalImage(-1, image2)
+            }
+
+            if (orderArray.contains(imageId)) {
+                originalsArraySize[orderArray.indexOf(imageId)] =
+                    OriginalImage(imageId, image2)
+            } else {
+                originalsArraySize += (OriginalImage(imageId, image2))
+            }
+        }
+
+        while (resultsCursor.moveToNext()) {
+            val imageId = resultsCursor.getInt(0)
+            val image: ByteArray =
+                resultsCursor.getBlob(resultsCursor.getColumnIndexOrThrow("image"))
+            val original = resultsCursor.getInt(3)
+            val image2: Bitmap = BitmapFactory.decodeByteArray(image, 0, image.size)
+            resultsArray += ResultsImage(imageId, image2, original)
+        }
     }
 
 }
